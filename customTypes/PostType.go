762c1fd5690f99424c3704789d917151b52162e7
Tej-11 <pavanteja.lalugani@everest.engineering>
2024-03-05 03:40:26 +0530

    Create type PostType

---
+++ customTypes/PostType.go
@@ -0,0 +1,11 @@
+package customTypes
+
+type PostType struct {
+	PostId              string
+	UserId              string
+	Username            string
+	ProfileImageAddress string
+	Images              []string
+	Caption             string
+	LikesCount          int
+}
