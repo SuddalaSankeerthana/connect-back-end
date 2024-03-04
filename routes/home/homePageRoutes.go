53a80b695ccc0e2629b3c29a329a10e2c604e1d1
Tej-11 <pavanteja.lalugani@everest.engineering>
2024-03-05 03:50:53 +0530

    Add controller GetPosts to route /homepage/get-post

--- routes/home/homePageRoutes.go
+++ routes/home/homePageRoutes.go
@@ -1,14 +1,11 @@
 package home
 
 import (
+	"github.com/Tej-11/connect-backend-application/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func Routes(route *gin.Engine) {
 	home := route.Group("/homepage")
-	home.GET("/get-posts", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "All posts",
-		})
-	})
+	home.GET("/get-posts", controllers.GetPosts)
 }
