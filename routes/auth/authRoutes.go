9df02888cc00963f17f9cbdd843b3d48a74a9b0a
Tej-11 <pavanteja.lalugani@everest.engineering>
2024-03-05 04:32:39 +0530

    Add post routes for /auth/login and /auth/sing-up

--- routes/auth/authRoutes.go
+++ routes/auth/authRoutes.go
@@ -8,4 +8,30 @@ import (
 
 func Routes(route *gin.Engine) {
 	auth := route.Group("/auth")
+    auth.POST("/login", func(c *gin.Context) {
+
+		var requestBody struct {
+			Message string `json:"message"`
+		}
+
+		if err := c.BindJSON(&requestBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "You are in the login route"})
+	})
+	auth.POST("/sign-up", func(c *gin.Context) {
+
+		var requestBody struct {
+			Message string `json:"message"`
+		}
+
+		if err := c.BindJSON(&requestBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "You are in the sign-up route"})
+	})
 }
