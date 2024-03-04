9f1e82e349db089760b2abbb710557707d24ec3f
Tej-11 <pavanteja.lalugani@everest.engineering>
2024-03-05 03:39:57 +0530

    Create type ImagePostUserDetailsType

---
+++ customTypes/ImagePostUserDetailsType.go
@@ -0,0 +1,11 @@
+package customTypes
+
+type ImagePostUser struct {
+	UserId              string
+	PostId              string
+	Username            string
+	ProfileImageAddress string
+	ImageURL            string
+	Caption             string
+	LikesCount          int
+}
