836b86f23d3061be705c226eadda0af8571d4fee
Tej-11 <pavanteja.lalugani@everest.engineering>
2024-03-05 03:52:42 +0530

    Add routes group homepage to route

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"github.com/Tej-11/connect-backend-application/database/config"
 	"github.com/Tej-11/connect-backend-application/database/queries"
+	"github.com/Tej-11/connect-backend-application/routes/home"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.EstablishDatabaseConnection()
 	queries.CreateSeeds()
+	router := gin.Default()
+	home.Routes(router)
+	router.Run()
 }
