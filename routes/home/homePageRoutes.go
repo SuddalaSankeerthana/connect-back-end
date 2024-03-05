9e6a09fec07184cfee4ce61fa9f01670efff9707
Tej-11 <pavanteja.lalugani@everest.engineering>
2024-03-05 23:24:39 +0530

    Add controller to the route /homepage/get-comments

--- routes/home/homePageRoutes.go
+++ routes/home/homePageRoutes.go
@@ -8,9 +8,5 @@ import (
 func Routes(route *gin.Engine) {
 	home := route.Group("/homepage")
 	home.GET("/get-posts", controllers.GetPosts)
-	home.GET("/get-comments", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "You are on the get-comments route",
-		})
-	})
+	home.GET("/get-comments", controllers.GetComments)
 }
