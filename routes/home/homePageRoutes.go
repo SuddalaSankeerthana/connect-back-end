89386613af710344cc68194333733e74563a40f1
Tej-11 <pavanteja.lalugani@everest.engineering>
2024-03-05 13:14:02 +0530

    Create route /homepage/get-comments

--- routes/home/homePageRoutes.go
+++ routes/home/homePageRoutes.go
@@ -8,4 +8,9 @@ import (
 func Routes(route *gin.Engine) {
 	home := route.Group("/homepage")
 	home.GET("/get-posts", controllers.GetPosts)
+	home.GET("/get-comments", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "You are on the get-comments route",
+		})
+	})
 }
