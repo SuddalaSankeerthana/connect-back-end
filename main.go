d8a4bd9a5c2ba702ec0f487ed1cbd092424abbd1
Tej-11 <pavanteja.lalugani@everest.engineering>
2024-03-05 11:20:56 +0530

    Add cors to router

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Tej-11/connect-backend-application/routes/auth"
 	"github.com/Tej-11/connect-backend-application/routes/home"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +14,7 @@ func main() {
 	config.EstablishDatabaseConnection()
 	queries.CreateSeeds()
 	router := gin.Default()
+	router.Use(cors.Default())
 	home.Routes(router)
 	auth.Routes(router)
 	router.Run()
