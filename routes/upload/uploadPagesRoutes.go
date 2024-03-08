5481a2becf054e1541f293991abd94f484255fe0
deepikaChippishetti <deepika.chippishetti@everest.engineering>
2024-03-08 13:51:42 +0530

    Create end point: /upload/upload-post

--- routes/upload/uploadPagesRoutes.go
+++ routes/upload/uploadPagesRoutes.go
@@ -6,4 +6,10 @@ import (
 
 func Routes(routes *gin.Engine) {
 	upload := routes.Group("/upload")
+
+	upload.POST("/upload-post", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "This is upload route",
+		})
+	})
 }
