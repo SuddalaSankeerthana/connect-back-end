649759e2d5edb117177fe8b6f97d286f3a1b721b
deepikaChippishetti <deepika.chippishetti@everest.engineering>
2024-03-08 15:01:16 +0530

    Add Upload Post controller to end point /upload/upload-post

--- routes/upload/uploadPagesRoutes.go
+++ routes/upload/uploadPagesRoutes.go
@@ -1,15 +1,12 @@
 package upload
 
 import (
+	"github.com/Tej-11/connect-backend-application/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func Routes(routes *gin.Engine) {
 	upload := routes.Group("/upload")
 
-	upload.POST("/upload-post", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "This is upload route",
-		})
-	})
+	upload.POST("/upload-post", controllers.UploadPost)
 }
